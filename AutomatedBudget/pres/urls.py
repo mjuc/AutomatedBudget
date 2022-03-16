a56aa9436204c26ad584d4992f48013f3c398ea8
mjuc <michal.jucewicz@protonmail.com>
2022-03-16 20:43:38 +0100

    Project restart.

--- AutomatedBudget/pres/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views 
-    
-urlpatterns = [
-        path('', views.index),
-        path('login/', views.signin),
-        path('logout/', views.signout),
-        path('signup/', views.signup),
-]
