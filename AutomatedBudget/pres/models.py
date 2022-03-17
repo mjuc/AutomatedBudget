03ca11540201f5d77a8040ce4b47906c959562ca
mjuc <michal.jucewicz@protonmail.com>
2022-03-17 21:02:41 +0100

    Added basis of data model for budget of different types.

--- AutomatedBudget/pres/models.py
+++ AutomatedBudget/pres/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Budget():
+    TYPES = [('MONTH','monthly'),('QUART','quarterly'),('YEAR','yearly')]
+
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    creation_date = models.DateField()
+    type = models.CharField(max_length=5,choices=TYPES)
