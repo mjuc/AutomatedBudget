49cb0543a1d556bd41775c8eae0d93fc52f1ee30
mjuc <michal.jucewicz@protonmail.com>
2022-03-20 21:01:24 +0100

    Finished budget data model.

--- AutomatedBudget/pres/models.py
+++ AutomatedBudget/pres/models.py
@@ -1,9 +1,19 @@
+from unicodedata import category
 from django.db import models
 from django.contrib.auth.models import User
 
-class Budget():
+class Category(models.Model):
+    name = models.CharField()
+
+class Expense(models.Model):
+    sum = models.CommaSeparatedIntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+class Budget(models.Model):
     TYPES = [('MONTH','monthly'),('QUART','quarterly'),('YEAR','yearly')]
 
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     creation_date = models.DateField()
     type = models.CharField(max_length=5,choices=TYPES)
+    expenses = models.ForeignKey(Expense, on_delete=models.CASCADE)
+    income = models.CommaSeparatedIntegerField()
