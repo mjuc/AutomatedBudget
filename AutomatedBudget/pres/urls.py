f2def3125024b4e82e7f23a88623dfb25d305003
mjuc <michal.jucewicz@protonmail.com>
2022-04-03 16:30:56 +0200

    Added functional landing page.

--- AutomatedBudget/pres/urls.py
+++ AutomatedBudget/pres/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    path('',views.landing),
     path('history/', views.history, name="history"),
     path('current/', views.current, name="current"),
     path('create/', views.create)
--- AutomatedBudget/pres/views.py
+++ AutomatedBudget/pres/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+def landing(request):
+    return render(request,'budgets/landing_page.html')
+
 @login_required
 def history(request):
     return render(request,'budgets/historic.html')
