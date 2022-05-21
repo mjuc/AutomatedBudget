bc55c9fd670998c546f9123719932606adc4715d
mjuc <michal.jucewicz@protonmail.com>
2022-05-21 17:48:23 +0200

    Added notification if budget cycle is ending.

---
+++ AutomatedBudget/static/notifications.js
@@ -0,0 +1,114 @@
+const m30 = [3,5,8,10];
+const m31 = [0,2,4,6,7,9,11];
+
+function getMonthNumber(month){
+    if (month.includes("Jan")){
+        return 0
+    }
+    else if(month.includes("Feb")){
+        return 1
+    }
+    else if(month.includes("Mar")){
+        return 2
+    }
+    else if(month.includes("Apr")){
+        return 3
+    }
+    else if(month.includes("May")){
+        return 4
+    }
+    else if(month.includes("Jun")){
+        return 5
+    }
+    else if(month.includes("Jul")){
+        return 6
+    }
+    else if(month.includes("Aug")){
+        return 7
+    }
+    else if(month.includes("Sep")){
+        return 8
+    }
+    else if(month.includes("Oct")){
+        return 9
+    }
+    else if(month.includes("Nov")){
+        return 10
+    }
+    else if(month.includes("Dec")){
+        return 11
+    }
+}
+
+function parseDate(date){
+    arr = date.split(" ");
+    let day = arr[0];
+    let year = arr[2];
+    let month = getMonthNumber(arr[1]);
+    var d = new Date();
+    d.setFullYear(year,month,day);
+    return d
+}
+
+function showExpiringNotification(){
+    const creationDateText = document.getElementById("creation_date").innerText;
+    const creationDate = parseDate(creationDateText);
+    const type = document.getElementById("type").innerText;
+    let currentDate = new Date();
+    var timeDiff = Math.abs(dateSecond.getTime() - dateFirst.getTime());
+    var daysDiff = Math.ceil(timeDiff / (1000 * 3600 * 24));
+    let message = "";
+    let days = 0;
+    if (type == "MONTH"){
+        if (currentDate.getMonth() == 1){
+            if (daysDiff >= 23){
+                days = 28 - daysDiff;
+            }
+        }
+        else{
+            if (daysDiff >=28){
+                if (m30.includes(currentDate.getMonth())){
+                    days = 30 - daysDiff;
+                }
+                else if (m31.includes(currentDate.getMonth())){
+                    days = 31 - daysDiff;
+                }
+            }
+        }
+
+    }
+    else if (type == "QUART"){
+        if (daysDiff >= 87){
+            if (currentDate.getMonth >= 9){
+                days = 92 - daysDiff;
+            }
+            else{
+                days = 91 -daysDiff;
+            }
+        }
+    }
+    else if (type == "YEAR"){
+        if (daysDiff >= 359){
+            if ((currentDate.getFullYear() % 4) == 0){
+                days = 366 - daysDiff;
+            }
+            else{
+                days = 365 - daysDiff;
+            }
+        }
+    }
+    if(days == 0){
+        message += "Budget expires today.";
+    }
+    else if (days < 0){
+        message += "Budget expired " + Math.abs(days) + " days ago.";
+    }
+    else{
+        message += "Budget expires in " + days + " days.";
+    }
+    if (days != 0){
+        alert(message);
+    }
+}
+
+showExpiringNotification()
