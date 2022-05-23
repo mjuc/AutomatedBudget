579eb37114b0d4dd19c6cf933c3c0214cbfefeea
mjuc <michal.jucewicz@protonmail.com>
2022-05-23 20:37:54 +0200

    Created endpoint view.

--- AutomatedBudget/api/views.py
+++ AutomatedBudget/api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from rest_framework.views import APIView
+from pres import models
 
-# Create your views here.
+@login_required
+class ExpenseUpdateView(APIView):
+    def post(self, request):
+        savings = models.Savings.objects.filter(owner=request.user)
+        expense = models.Expense.objects.filter(id=request.data.exp_id)
+        expense.spent_sum += request.data.expSpentSum
+        savings.saved_sum -= request.data.expSpentSum
+        expense.save()
+        savings.save()
