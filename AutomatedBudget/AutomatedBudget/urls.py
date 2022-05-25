9e61ad52cdad34c4afb25603e4aa210d9af1cb0e
mjuc <michal.jucewicz@protonmail.com>
2022-05-25 16:44:09 +0200

    Removed redundant imports.

--- AutomatedBudget/AutomatedBudget/urls.py
+++ AutomatedBudget/AutomatedBudget/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
-from django.views.decorators.csrf import ensure_csrf_cookie
 from . import views
  
 urlpatterns = [
--- AutomatedBudget/pres/budget_creation.py
+++ AutomatedBudget/pres/budget_creation.py
@@ -1,4 +1,4 @@
-from random import randint, random,uniform,randrange
+from random import uniform,randrange
 from numpy import floor
 
 
